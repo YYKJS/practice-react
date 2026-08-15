8f12bfbfd28d44598c92ff0516d444fa98a5b0ec
unknown <you@example.com>
2026-08-15 14:40:52 +0900

    update test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 function App() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>初めてのReactアプリ</h1>
-      <p>VS Codeで書き換えたら、ブラウザが自動で更新されます！</p>
-    </div>
+      <h1>Reactアプリ</h1>
   );
 }
 
